acf46972b10ff5cad0ddc9045075b78f520bc872
Joe Gesualdo <joegesualdo@gmail.com>
2014-08-29 17:17:27 -0400

    Create a fetch request for the JGBucketListItems; encapsulate it in a method

---
+++ JGBucketList/JGBucketListViewController.h
@@ -0,0 +1,15 @@
+//
+//  JGBucketListViewController.h
+//  JGBucketList
+//
+//  Created by Joe Gesualdo on 8/29/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGBucketListViewController : UITableViewController
+
+-(NSFetchRequest *)itemListFetchRequest;
+
+@end
---
+++ JGBucketList/JGBucketListViewController.m
@@ -0,0 +1,134 @@
+//
+//  JGBucketListViewController.m
+//  JGBucketList
+//
+//  Created by Joe Gesualdo on 8/29/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import "JGBucketListViewController.h"
+#import "JGCoreDataStack.h"
+
+@interface JGBucketListViewController ()
+
+@end
+
+@implementation JGBucketListViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+// responsible for creating and configuring a fetch request to fetch all the items in our core data store sorted by title
+// we put the fetch request in a method so it can be tested independently of actually fetching data
+-(NSFetchRequest *)itemListFetchRequest
+{
+    // this will create a fetch request for the entity JGBucketListEntry.
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"JGBucketListEntry"];
+    
+    // Every fetch request MUST have a predicate or a sort descriptor, so we implement a sort descriptor that sorts the items by the title
+    // you can pass in more sort than one sort desciption, you just pass them in as an array
+    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"title" ascending:NO]];
+    
+    return fetchRequest;
+}
+
+@end
