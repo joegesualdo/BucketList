faf092ec62ed27488ad05bab2eafbc4ce21f64d7
Joe Gesualdo <joegesualdo@gmail.com>
2014-08-30 13:06:56 -0400

    Style status bar; make the status bar content white

---
+++ JGBucketList/JGNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  JGNavigationController.h
+//  JGBucketList
+//
+//  Created by Joe Gesualdo on 8/30/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGNavigationController : UINavigationController
+
+@end
---
+++ JGBucketList/JGNavigationController.m
@@ -0,0 +1,23 @@
+//
+//  JGNavigationController.m
+//  JGBucketList
+//
+//  Created by Joe Gesualdo on 8/30/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import "JGNavigationController.h"
+
+@interface JGNavigationController ()
+
+@end
+
+@implementation JGNavigationController
+
+// Sets the style for the status bar
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    // makes everything in the status bar white;
+    return UIStatusBarStyleLightContent;
+}
+
+@end
